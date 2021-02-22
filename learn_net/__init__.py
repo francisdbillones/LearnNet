969f1c776d22a3bbd32a6f31dfc7812de21b49da
francisdbillones <francisd.billones@gmail.com>
2021-02-22 17:27:05 +0800

    added functionality to create sensitive content folders such as images and user's kits

--- learn_net/__init__.py
+++ learn_net/__init__.py
@@ -36,9 +36,17 @@ Session(app)
 
 # create images folder
 images_path = os.path.join(app.root_path, 'learn_net', 'static', 'images')
-if not os.path.isdir(image_path):
+if not os.path.isdir(images_path):
     # create profile_pictures folder
-    if not os.path.isdir
+    profile_pictures_path = os.path.join(app.root_path, images_path, 'profile_picutres')
+    if not os.path.isdir(profile_pictures_path):
+        os.makedirs(profile_pictures_path)
+    os.makedirs(images_path)
+
+# create user_kits folder
+user_kits_path = os.path.join(app.root_path, 'learn_net', 'static', 'user_kits')
+if not os.path.isdir(user_kits_path):
+    os.makedirs(user_kits_path)
 
 # Ensure responses aren't cached
 @app.after_request
