f05fdd9c953a071011545c8294d68ba9235af0a6
francisdbillones <francisd.billones@gmail.com>
2021-05-07 21:08:46 +0800

    serve with waitress instead of builtin server

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     import os
 
     dev = os.environ.get('DEV')
-
     if dev:
         app.run(debug=True)
     else:
-        app.run()
+        from waitress import serve
+        serve(app, host='0.0.0.0', port=8080)
