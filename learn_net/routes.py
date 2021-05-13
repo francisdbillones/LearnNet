1a2ee089c514fc86d2ad4d458eddc3f22ecbd510
francisdbillones <francisd.billones@gmail.com>
2021-05-13 09:37:38 +0800

    remove unused import

--- learn_net/routes.py
+++ learn_net/routes.py
@@ -1,4 +1,3 @@
-from operator import add
 from flask import (flash, jsonify, redirect, render_template, request,
                    url_for)
 from flask_login import current_user, login_required, login_user, logout_user
